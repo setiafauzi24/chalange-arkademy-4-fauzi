d682d4044c66bb87affa7695006e2ae38dec7d14
uzibangke <59769476+uzibangke@users.noreply.github.com>
2020-01-18 22:00:34 +0700

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,11 +1,23 @@
-function deretBilang (x,y){
-let x = 0;
+
+function deretBilangan(x,y){
   if (typeof(x) !='number' && typeof(y) != 'number'){
     return 'parameter bertype number'
   } else if ( x < y){
     return 'parameter x harus lebih besar dibanding y'
   }
 
+let pangkat = 0;
+let hasil= = 
+let logik = false
+
+
+for (let i =0;hasil !=0  || hasil !=1 && logic !=false; i++ ){
+logik +=true
+  x+=1
+  pangkat+=1
+   hasil =(y* pangkat) % x
+console.log(hasil)
+}
  return true
 
 
